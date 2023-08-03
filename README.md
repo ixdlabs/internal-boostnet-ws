efb4fec4d697d682e7a339250f49086823afc087
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2023-08-03 13:27:49 +0530

    Fix readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ```shell
 cd rabbitmq
 docker build -t rabbitmq .
-docker run -p 5432:5432 -d --name rabbitmq rabbitmq
+docker run -p 5432:5432 -p 15672:15672 -d --name rabbitmq rabbitmq
 ```
 
 ### WS Service
--- src/boostnet_ws/config.py
+++ src/boostnet_ws/config.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 PORT = int(os.getenv("PORT", "3000"))
 HOST = os.getenv("HOST", "0.0.0.0")
-AMQP_URL = os.getenv("AMQP_URL", "amqp://127.0.0.1/")
+AMQP_URL = os.getenv(
+    "AMQP_URL",
+    "amqp://guest:guest@localhost:5672/%2F?connection_attempts=20&retry_delay=1",
+)
 CHARGE_POINT_ID = "charge_point_id"
 RPC_SEND_QUEUE = "rpc"
